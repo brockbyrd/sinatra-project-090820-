47772defa3726a0245559480dddbe6a8d3356863
brockbyrd <brockbyrdd@gmail.com>
2020-10-23 11:51:22 -0500

    'sets routes for users controller'

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -3,5 +3,44 @@ class UsersController < ApplicationController
     get '/' do
         erb :index
     end
+ 
+    get '/signup' do
+        if params.blank?
+            redirect '/signup'
+        else
+            @user = User.create(:username => params[:username], :email => params[:email], :password => params[:password])
+            sesssion[:user_id] = @user.id
+            redirect '/posts'
+        end
+    end
+
+    get '/login' do
+        if !logged_in?
+            erb :'/users/login'
+        else
+            redirect '/posts'
+        end
+    end
+
+    post '/login' do
+        user = User.find_by(:username => params[:username])
+        if user && user.authenticate(params[:password])
+            sesssion[:user_id] = user.id
+            redirect '/posts'
+        else
+            redirect '/signup'
+        end
+    end
+
+
+    get '/users/:slug' do
+        User.find_by_slug(params[:username])
+        erb :'users/show'
+    end
+
+    get '/logout' do
+        session.clear
+        redirect '/login'
+    end
 
 end
