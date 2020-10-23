d62e6882666bc8cb3ae4c37e68a255b9b887610d
brockbyrd <brockbyrdd@gmail.com>
2020-10-23 12:16:56 -0500

    "sets /posts route"

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -1,4 +1,10 @@
 class PostsController < ApplicationController
 
+    get '/posts' do
+        if !authenticate
+            @posts = Posts.all
+            erb :'/posts/posts'
+        end
+    end
 
 end
