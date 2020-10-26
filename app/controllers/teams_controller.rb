17a602d28223b7c1ac761288109d6e9881412cfc
brockbyrd <brockbyrdd@gmail.com>
2020-10-26 15:13:57 -0500

    "sets routes for team controller"

--- app/controllers/teams_controller.rb
+++ app/controllers/teams_controller.rb
@@ -1,4 +1,34 @@
 class TeamsController < ApplicationController
 
+    get '/teams' do
+        if !authenticate
+            @teams = Team.all
+            erb :'/teams/teams'
+        end
+    end
+
+    get '/teams/new' do
+        if !authenticate
+            erb :'teams/new'
+        end
+    end
+
+    post '/teams' do
+        if !authenticate
+            @team = Team.build(city: params[:city], name: params[:name])
+                if @team.save
+                    redirect '/teams'
+                else
+                    redirect '/teams/new'
+                end
+        end
+    end
+
+    get '/teams/:id' do
+        if !authenticate
+            @team = Team.find_by_id(params[:id])
+            erb :'/teams/show'
+        end
+    end
 
 end
